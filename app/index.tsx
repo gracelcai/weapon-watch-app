69a327dbb153ab066b0d058739e2708260fc069c
chrissu777 <christophersu777@gmail.com>
2024-12-29 17:21:21 -0800

    changed index so i can actaully see my login screeN

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+import LoginForm from "./screens/login";
+import React from "react";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+    <LoginForm/>
+    </>
   );
 }
