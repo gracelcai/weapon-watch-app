59713433450bfa980728ed2667bfd7e531e7704e
keguida <keguida11@gmail.com>
2025-01-16 21:14:19 -0500

    updated notification system

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,48 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { NotificationProvider } from '@/context/NotificationContext';
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NotificationProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </ThemeProvider>
+    </NotificationProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Platform, SafeAreaView, StatusBar } from "react-native";
+import {ThemedText} from "@/components/ThemedText";
+import {ThemedView} from "@/components/ThemedView";
+import {useNotification} from "@/context/NotificationContext";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+export default function HomeScreen() {
+  const {notification, expoPushToken, error} = useNotification();
+  
+  if(error){
+    return <ThemedText>Error: {error.message}</ThemedText>;
+  }
+  console.log(JSON.stringify(notification, null, 2));
+  return(
+    <ThemedView 
+      style={{flex: 1,
+              padding: 10,
+              paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 10,
+      }}>
+        <SafeAreaView style={{ flex: 1}}>
+        <ThemedText type-="subtitle">Your push token:</ThemedText>
+        <ThemedText>{expoPushToken}</ThemedText>
+        <ThemedText type="subtitle">Latest notification:</ThemedText>
+        <ThemedText>{notification?.request.content.title}</ThemedText>
+        <ThemedText>
+          {JSON.stringify(notification?.request.content.data, null, 2)}
+        </ThemedText>
+        </SafeAreaView>
+    </ThemedView>
   );
 }
+  
+ 
+  
+ 
