41bb2def8571096fa3f0dfbcf572257f0c639a5b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-01-27 23:06:27 -0500

    Revert "ignore error"
    This reverts commit e7d8ecd60590eb7f1eacb9f887e51243e1db7a36.

--- camera/record.py
+++ camera/record.py
@@ -1,5 +1,3 @@
-import cv2 # type: ignore
-
 def record(q):
     while True:
         if not q.empty():
@@ -8,22 +6,3 @@ def record(q):
                 break
             else:
                 print(key)
-                
-                cap = cv2.VideoCapture('rtsp://rtspstream:oG2yLaie9XWG-LBgjEoUs@zephyr.rtsp.stream/movie')
-                if(cap.isOpened)==False:
-                    print("Error while reading the file")
-                    
-                fourcc = cv2.VideoWriter_fourcc(*'MP42')
-                out = cv2.VideoWriter('footage.mp4', fourcc, 20.0, (720, 480), True)
-                
-                while(True):
-                    ret,photo=cap.read()
-                    out.write(photo)
-                    key = cv2.waitKey(1)
-                    if key == ord('q'):
-                        break
-                
-                cap.release()
-                cv2.destroyAllWindows()
-                
-                print("\nSuccessfully Downloaded Footage")
