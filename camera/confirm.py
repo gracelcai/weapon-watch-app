7d5fa1b47cf3ae74c6436a315b2d6010c3765319
keguida <keguida11@gmail.com>
2025-03-17 13:42:01 -0400

    Updated notification system

--- camera/confirm.py
+++ camera/confirm.py
@@ -1,4 +1,12 @@
 import json
+# You need to pip install exponent_server_sdk
+from exponent_server_sdk import (
+    DeviceNotRegisteredError,
+    PushClient,
+    PushMessage,
+    PushServerError,
+    PushTicketError,
+)
 
 def confirm(q): 
     while True:
@@ -8,12 +16,26 @@ def confirm(q):
                 break
             else:
                 status = json.load(open('status.json'))
-                
-                status['detected'] = True
+                #replace this token with the expo token you get from running the app on your phone with npx expo start
+                push_token = "ExponentPushToken[eBiWRAPTeF1i5Wgm8M4tyC]"
+
+                message = PushMessage(
+                    to=push_token,
+                    channel_id="weapon_detected",
+                    sound="emergencysos.wav",
+                    title="Weapon Detected!",
+                    body="A potential weapon has been detected. Please check immediately."
+                )
                     
                 # for box in key:
                 #     print(box)
-                    
+                try:
+                    if status['detected'] is False:
+                        response = PushClient().publish(message)
+                        print("Notification sent:", response)
+                except PushResponseError as exc:
+                    print("Error sending notification:", exc.errors)
+                status['detected'] = True                    
                 status['confirmed'] = True
                 
                 with open('status.json', 'w') as f:
